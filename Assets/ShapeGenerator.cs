8d9734d84889f4ee2f0441ac7bbdf43db8c3860a
Max Bremer <wmaxonbremer@gmail.com>
2020-01-07 15:42:43 -0600

    Added Unity proj files, all work so far.

---
+++ Assets/ShapeGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeGenerator
+{
+    ShapeSettings settings;
+    iNoiseFilter[] noiseFilters;
+
+    public ShapeGenerator(ShapeSettings settings)
+    {
+        this.settings = settings;
+        noiseFilters = new iNoiseFilter[settings.noiseLayers.Length];
+        for (int i = 0; i < noiseFilters.Length; i++)
+        {
+            noiseFilters[i] = NoiseFilterFactory.CreateNoiseFilter(settings.noiseLayers[i].noiseSettings);
+        }
+    }
+
+    public Vector3 CalculatePointOnPlanet(Vector3 ptOnUnitSphere)
+    {
+        float firstLayerValue = 0;
+        float elevation = 0;
+
+        if (noiseFilters.Length > 0)
+        {
+            firstLayerValue = noiseFilters[0].Evaluate(ptOnUnitSphere);
+            if (settings.noiseLayers[0].enabled)
+            {
+                elevation = firstLayerValue;
+            }
+        }
+        for (int i = 1; i < noiseFilters.Length; i++)
+        {
+            if (settings.noiseLayers[i].enabled)
+            {
+                float mask = settings.noiseLayers[i].useFirstLayerAsMask ? firstLayerValue : 1;
+                elevation += noiseFilters[i].Evaluate(ptOnUnitSphere) * mask;
+            }
+        }
+        return ptOnUnitSphere * settings.planetRadius * (1 + elevation);
+    }
+}
